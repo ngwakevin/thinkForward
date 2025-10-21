d384185eb8d5bb0139d3d3aa2cb13a19cf472657
ngwa kevin <kevin.ngwa@kemin.com>
2025-10-21 16:32:37 +0200

    chore(repo): remove backup/temp files and update .gitignore to ignore them

--- pages.backup/_app.js
+++
@@ -1,8 +0,0 @@
-// Force CommonJS format for Next.js compatibility
-require('../app/globals.css');
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-module.exports = MyApp;
